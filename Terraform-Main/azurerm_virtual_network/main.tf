bf07aeba2ee6f8923fa03095f7e677c2d1ac488b
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-02 19:02:24 +0530

    Update main.tf

--- Terraform-Main/azurerm_virtual_network/main.tf
+++ Terraform-Main/azurerm_virtual_network/main.tf
@@ -1,50 +1,27 @@
-variable "resourcegroup_name" {
-  type        = string
-  description = "The name of the resource group"
-  default     = "terraform-deployment-rg"
-}
-
-variable "location" {
-  type        = string
-  description = "The region for the deployment"
-  default     = "Central India"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "Tags used for the deployment"
-  default = {
-    "Environment" = "Lab"
-    "Owner"       = "Tejas"
-  }
-}
-
-variable "vnet_name" {
-  type        = string
-  description = "The name of the vnet"
-  default     = "VNET-Terraform"
-}
-
-variable "vnet_address_space" {
-  type        = list(any)
-  description = "the address space of the VNet"
-  default     = ["10.13.0.0/16"]
-}
-
-variable "subnets" {
-  type = map(any)
-  default = {
-    subnet_1 = {
-      name             = "subnet_1"
-      address_prefixes = ["10.13.1.0/24"]
-    }
-    subnet_2 = {
-      name             = "subnet_2"
-      address_prefixes = ["10.13.2.0/24"]
-    }
-    subnet_3 = {
-      name             = "subnet_3"
-      address_prefixes = ["10.13.3.0/24"]
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.110.0"
     }
   }
+backend "azurerm" {}
+}
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = data.azurerm_resource_group.resource_group_name.location
+  resource_group_name = data.azurerm_resource_group.resource_group_name.name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  for_each = var.subnets
+  resource_group_name  = data.azurerm_resource_group.resource_group_name.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  name                 = each.value["name"]
+  address_prefixes     = each.value["address_prefixes"]
 }
