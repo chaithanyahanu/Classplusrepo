0a6eeb9050cee8dac08ff05669041ac815c7425d
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-02 17:47:32 +0530

    Update main.tf

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.location
