1a9d19bd44d7a4cf0bc9be4b26e98d555bcb9815
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-02 12:43:13 +0530

    Update variable.tf

--- azurerm_resource_group/variable.tf
+++ azurerm_resource_group/variable.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
   type    = string
-  default = ""
+  default = "classplus-prod-RG"
 }
 
 variable "location" {
   type    = string
-  default = ""
+  default = "East US"
 }
 variable "tags" {
   description = "A map of tags to add to all resources"
