7ec90510d4971d8f246e57afcfd906bf9265cfdc
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-02 18:46:29 +0530

    Update variable.tf

--- Terraform-Main/azurerm_virtual_network/variable.tf
+++ Terraform-Main/azurerm_virtual_network/variable.tf
@@ -1,34 +1,13 @@
-variable "resourcegroup_name" {
-  type        = string
-  description = "The name of the resource group"
-  default     = "terraform-deployment-rg"
-}
-
-variable "location" {
-  type        = string
-  description = "The region for the deployment"
-  default     = "Central India"
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "Tags used for the deployment"
-  default = {
-    "Environment" = "Lab"
-    "Owner"       = "Tejas"
-  }
-}
-
 variable "vnet_name" {
   type        = string
   description = "The name of the vnet"
-  default     = "VNET-Terraform"
+  default     = "prod-vnet"
 }
 
 variable "vnet_address_space" {
   type        = list(any)
   description = "the address space of the VNet"
-  default     = ["10.13.0.0/16"]
+  default     = ["10.14.0.0/16"]
 }
 
 variable "subnets" {
@@ -36,15 +15,15 @@ variable "subnets" {
   default = {
     subnet_1 = {
       name             = "subnet_1"
-      address_prefixes = ["10.13.1.0/24"]
+      address_prefixes = ["10.14.7.0/24"]
     }
     subnet_2 = {
       name             = "subnet_2"
-      address_prefixes = ["10.13.2.0/24"]
+      address_prefixes = ["10.14.8.0/24"]
     }
     subnet_3 = {
       name             = "subnet_3"
-      address_prefixes = ["10.13.3.0/24"]
+      address_prefixes = ["10.14.9.0/24"]
     }
   }
 }
