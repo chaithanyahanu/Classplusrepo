15ed7834ced9989694f6e93806a275ef945d330b
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-09 15:07:14 +0530

    Update main.tf

--- Terraform-Main/azurerm_virtual_network/main.tf
+++ Terraform-Main/azurerm_virtual_network/main.tf
@@ -1,35 +1,34 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.1.0"
+      source = "hashicorp/azurerm"
+      version = "3.110.0"
     }
   }
-  backend "azurerm" {}
+backend "azurerm" {}
 }
-
 provider "azurerm" {
   features {}
 }
 
-# Data block for existing resource group
-data "azurerm_resource_group" "Rgexisting" {
-  name = "classplus-prod-RG"  # Assuming this is the correct resource group name
+resource "azurerm_resource_group" "vnet_rg" {
+  name     = var.resourcegroup_name
+  location = var.location
+  tags     = var.tags
 }
 
-# Virtual Network (VNet) resource
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
-  location            = data.azurerm_resource_group.Rgexisting.location  # Correct reference here
-  resource_group_name = data.azurerm_resource_group.Rgexisting.name      # Correct reference here
+  location            = azurerm_resource_group.vnet_rg.location
+  resource_group_name = azurerm_resource_group.vnet_rg.name
+  tags                = var.tags
 }
 
-# Subnet resource
 resource "azurerm_subnet" "subnet" {
-  for_each            = var.subnets
-  resource_group_name = data.azurerm_resource_group.Rgexisting.name
+  for_each = var.subnets
+  resource_group_name  = var.resourcegroup_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  name                = each.value["name"]
-  address_prefixes    = each.value["address_prefixes"]
+  name                 = each.value["name"]
+  address_prefixes     = each.value["address_prefixes"]
 }
