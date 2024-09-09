3a433a69fbe5942c2dc825ba725e5ca002ca202b
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-09 18:39:14 +0530

    Update variable.tf

--- Terraform-Main/azurerm_AI_search/variable.tf
+++ Terraform-Main/azurerm_AI_search/variable.tf
@@ -1,23 +1,18 @@
 
 variable "search_service_name" {
-  description = "The name of the Azure Cognitive Search service."
+  description = "The name of the Azure Search Service"
   type        = string
-  default     = "classplusaisearch"
-}
-variable "sku" {
-  description = "The SKU for the search service."
-  type        = string
-  default     = "standard"
+  default     = "classplsaisearch"
 }
 
-variable "replicas" {
-  description = "The number of replicas for the search service."
+variable "replica_count" {
+  description = "Number of replicas for the search service"
   type        = number
   default     = 1
 }
 
-variable "partitions" {
-  description = "The number of partitions for the search service."
+variable "partition_count" {
+  description = "Number of partitions for the search service"
   type        = number
   default     = 1
 }
