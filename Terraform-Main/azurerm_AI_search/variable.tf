c4a1a49531cf7639a9a3cfa7206380a65a53c9a5
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-09 18:21:54 +0530

    Update variable.tf

--- Terraform-Main/azurerm_AI_search/variable.tf
+++ Terraform-Main/azurerm_AI_search/variable.tf
@@ -4,3 +4,20 @@ variable "search_service_name" {
   type        = string
   default     = "classplusaisearch"
 }
+variable "sku" {
+  description = "The SKU for the search service."
+  type        = string
+  default     = "standard"
+}
+
+variable "replicas" {
+  description = "The number of replicas for the search service."
+  type        = number
+  default     = 1
+}
+
+variable "partitions" {
+  description = "The number of partitions for the search service."
+  type        = number
+  default     = 1
+}
