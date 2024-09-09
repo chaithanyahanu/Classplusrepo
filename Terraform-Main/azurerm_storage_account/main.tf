84703e33879980daa5fa7ef39cb80bb63471779b
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-09 15:51:18 +0530

    Update main.tf

--- Terraform-Main/azurerm_storage_account/main.tf
+++ Terraform-Main/azurerm_storage_account/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0" # Adjust the version according to your needs
+      version = "4.1.0" # Adjust the version according to your needs
     }
   }
 }
