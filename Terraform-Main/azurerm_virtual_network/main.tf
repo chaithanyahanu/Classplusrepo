daf1141ae46ecab6a7754532c0a14cc3c6e67c3f
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-09 11:43:25 +0530

    Update main.tf

--- Terraform-Main/azurerm_virtual_network/main.tf
+++ Terraform-Main/azurerm_virtual_network/main.tf
@@ -10,16 +10,19 @@ backend "azurerm" {}
 provider "azurerm" {
   features {}
 }
+data "azurerm_resource_group" "Rgexisting" {
+  name = "classplus-prod-RG"
+}
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
-  location            = var.location
-  resource_group_name = data.azurerm_resource_group.resource_group_name.name
+  location            = data.azurerm_resource_group.Rgexistinge.location
+  resource_group_name = data.azurerm_resource_group.Rgexistinge.name
 }
 
 resource "azurerm_subnet" "subnet" {
   for_each = var.subnets
-  resource_group_name  = data.azurerm_resource_group.resource_group_name.name
+  resource_group_name  = data.azurerm_resource_group.Rgexisting.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   name                 = each.value["name"]
   address_prefixes     = each.value["address_prefixes"]
