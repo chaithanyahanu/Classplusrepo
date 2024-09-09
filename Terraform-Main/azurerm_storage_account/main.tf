65cfd84e12dc61ebf50b7a6b6e38d2cd9535c6d0
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-09 15:39:24 +0530

    Update main.tf

--- Terraform-Main/azurerm_storage_account/main.tf
+++ Terraform-Main/azurerm_storage_account/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.1.0" # Adjust the version according to your needs
+      version = "~> 3.0" # Adjust the version according to your needs
     }
   }
 }
@@ -11,17 +11,16 @@ provider "azurerm" {
   features {}
 }
 
-# Resource group where the storage account will be deployed
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+# Data block to reference an existing resource group
+data "azurerm_resource_group" "existing_rg" {
+  name = "classplus-prod-RG"
 }
 
 # Storage account with ADLS Gen2
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = data.azurerm_resource_group.existing_rg.name
+  location                  = data.azurerm_resource_group.existing_rg.location
   account_tier              = var.account_tier
   account_replication_type  = var.account_replication_type
   is_hns_enabled            = true  # Enables hierarchical namespace for ADLS Gen2
