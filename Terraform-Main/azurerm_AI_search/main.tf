655402b9a86fa10b356d4d4f0382cadc7fc36b01
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-10 13:59:28 +0530

    Update main.tf

--- Terraform-Main/azurerm_AI_search/main.tf
+++ Terraform-Main/azurerm_AI_search/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_search_service" "example" {
   name                = var.search_service_name
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
-  sku                 = var.sku
+  sku                 = "standard"
   replicas            = var.replicas
   partitions          = var.partitions
 }
