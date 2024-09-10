d7a1b3543f42dba9757dc8fb76c8d92bf862c31e
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-10 14:12:10 +0530

    Update main.tf

--- Terraform-Main/azurerm_AI_search/main.tf
+++ Terraform-Main/azurerm_AI_search/main.tf
@@ -1,16 +1,16 @@
 provider "azurerm" {
   features {}
 }
-
+# Using an existing resource group
 data "azurerm_resource_group" "existing" {
   name = "classplus-prod-RG"
 }
-
+# Define Azure Cognitive Search Service
 resource "azurerm_search_service" "example" {
   name                = var.search_service_name
-  location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
   sku                 = "standard"
-  replicas            = var.replicas
-  partitions          = var.partitions
+  replica_count   = var.replica_count
+  partition_count = var.partition_count
 }
